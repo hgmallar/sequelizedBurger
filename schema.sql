542041a5e06ae0bed5e3334b1f7067bf59c2686b
Hilary Mallar <hilarymallar@gmail.com>
2018-12-21 15:05:08 -0500

    check in code

--- schema.sql
+++ schema.sql
@@ -1,12 +1,26 @@
 ### Schema
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+    createdAt timestamp NOT NULL,
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE eaters
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+    createdAt timestamp NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+
+ALTER TABLE burgers
+ADD COLUMN eater_id INT;
+
+LEFT OUTER JOIN eaters ON burgers.eater_id = eaters.id;
